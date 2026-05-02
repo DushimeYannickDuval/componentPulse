6ab206323d514d08395026144ea43a53d9d8d517
Yannick Dudu <dushimeyannick54@gmail.com>
2026-05-02 03:30:29 +0300

    Update page.tsx

--- src/app/(public)/support/page.tsx
+++ src/app/(public)/support/page.tsx
@@ -3,7 +3,7 @@ import { SupportView } from 'src/sections/support/view/support-view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Support Center | ComponentPulse',
+  title: 'Support Center | componentPulse',
 };
 
 export default function SupportPage() {
