3b34dc61630035de776163d3e8f3928ad6bfdc92
Yannick Dudu <dushimeyannick54@gmail.com>
2026-05-02 03:25:36 +0300

    Update page.tsx

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -5,7 +5,7 @@ import { HomeView } from 'src/sections/home/view';
 // ----------------------------------------------------------------------
 
 export const metadata: Metadata = {
-  title: 'ComponentPulse - Quality Electronic Components & Solar Solutions in Uganda',
+  title: 'componentPulse - Quality Electronic Components & Solar Solutions in Uganda',
   description:
     'Your trusted source for solar panels, inverters, batteries, and electrical components. Fast delivery across Uganda with competitive prices.',
 };
