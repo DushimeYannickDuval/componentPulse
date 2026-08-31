8d527cd8c092f954006150a08f991829a574ac58
Yannick Dudu <dushimeyannick54@gmail.com>
2026-08-31 15:09:01 +0300

    Update settings-drawer.tsx

--- src/components/settings/drawer/settings-drawer.tsx
+++ src/components/settings/drawer/settings-drawer.tsx
@@ -38,14 +38,14 @@ export function SettingsDrawer({ sx, defaultSettings }: SettingsDrawerProps) {
   // Visible options by default settings
   const visibility = {
     mode: hasKeys(defaultSettings, ['mode']),
-    contrast: hasKeys(defaultSettings, ['contrast']),
-    navColor: hasKeys(defaultSettings, ['navColor']),
-    fontSize: hasKeys(defaultSettings, ['fontSize']),
-    direction: hasKeys(defaultSettings, ['direction']),
-    navLayout: hasKeys(defaultSettings, ['navLayout']),
-    fontFamily: hasKeys(defaultSettings, ['fontFamily']),
-    primaryColor: hasKeys(defaultSettings, ['primaryColor']),
-    compactLayout: hasKeys(defaultSettings, ['compactLayout']),
+    // contrast: hasKeys(defaultSettings, ['contrast']),
+    // navColor: hasKeys(defaultSettings, ['navColor']),
+    // fontSize: hasKeys(defaultSettings, ['fontSize']),
+    // direction: hasKeys(defaultSettings, ['direction']),
+    // navLayout: hasKeys(defaultSettings, ['navLayout']),
+    // fontFamily: hasKeys(defaultSettings, ['fontFamily']),
+    // primaryColor: hasKeys(defaultSettings, ['primaryColor']),
+    // compactLayout: hasKeys(defaultSettings, ['compactLayout']),
   };
 
   useEffect(() => {
@@ -117,200 +117,200 @@ export function SettingsDrawer({ sx, defaultSettings }: SettingsDrawerProps) {
     />
   );
 
-  const renderContrast = () => (
-    <BaseOption
-      label="Contrast"
-      selected={settings.state.contrast === 'high'}
-      icon={<SvgIcon>{settingIcons.contrast}</SvgIcon>}
-      onChangeOption={() => {
-        settings.setState({
-          contrast: settings.state.contrast === 'default' ? 'high' : 'default',
-        });
-      }}
-    />
-  );
+  //const renderContrast = () => (
+  //   <BaseOption
+  //     label="Contrast"
+  //     selected={settings.state.contrast === 'high'}
+  //     icon={<SvgIcon>{settingIcons.contrast}</SvgIcon>}
+  //     onChangeOption={() => {
+  //       settings.setState({
+  //         contrast: settings.state.contrast === 'default' ? 'high' : 'default',
+  //       });
+  //     }}
+  //   />
+  // );
 
-  const renderDirection = () => (
-    <BaseOption
-      label="Right to left"
-      selected={settings.state.direction === 'rtl'}
-      icon={<SvgIcon>{settingIcons.alignRight}</SvgIcon>}
-      onChangeOption={() => {
-        settings.setState({ direction: settings.state.direction === 'ltr' ? 'rtl' : 'ltr' });
-      }}
-    />
-  );
+  // const renderDirection = () => (
+  //   <BaseOption
+  //     label="Right to left"
+  //     selected={settings.state.direction === 'rtl'}
+  //     icon={<SvgIcon>{settingIcons.alignRight}</SvgIcon>}
+  //     onChangeOption={() => {
+  //       settings.setState({ direction: settings.state.direction === 'ltr' ? 'rtl' : 'ltr' });
+  //     }}
+  //   />
+  // );
 
-  const renderCompactLayout = () => (
-    <BaseOption
-      tooltip="Dashboard only and available at large resolutions > 1600px (xl)"
-      label="Compact"
-      selected={!!settings.state.compactLayout}
-      icon={<SvgIcon>{settingIcons.autofitWidth}</SvgIcon>}
-      onChangeOption={() => {
-        settings.setState({ compactLayout: !settings.state.compactLayout });
-      }}
-    />
-  );
+  // const renderCompactLayout = () => (
+  //   <BaseOption
+  //     tooltip="Dashboard only and available at large resolutions > 1600px (xl)"
+  //     label="Compact"
+  //     selected={!!settings.state.compactLayout}
+  //     icon={<SvgIcon>{settingIcons.autofitWidth}</SvgIcon>}
+  //     onChangeOption={() => {
+  //       settings.setState({ compactLayout: !settings.state.compactLayout });
+  //     }}
+  //   />
+  // );
 
-  const renderPresets = () => (
-    <LargeBlock
-      title="Presets"
-      canReset={settings.state.primaryColor !== defaultSettings.primaryColor}
-      onReset={() => {
-        settings.setState({ primaryColor: defaultSettings.primaryColor });
-      }}
-    >
-      <PresetsOptions
-        icon={<SvgIcon sx={{ width: 28, height: 28 }}>{settingIcons.siderbarDuotone}</SvgIcon>}
-        options={(Object.keys(primaryColorPresets) as SettingsState['primaryColor'][]).map(
-          (key) => ({
-            name: key,
-            value: primaryColorPresets[key].main,
-          })
-        )}
-        value={settings.state.primaryColor}
-        onChangeOption={(newOption) => {
-          settings.setState({ primaryColor: newOption });
-        }}
-      />
-    </LargeBlock>
-  );
+  // const renderPresets = () => (
+  //   <LargeBlock
+  //     title="Presets"
+  //     canReset={settings.state.primaryColor !== defaultSettings.primaryColor}
+  //     onReset={() => {
+  //       settings.setState({ primaryColor: defaultSettings.primaryColor });
+  //     }}
+  //   >
+  //     <PresetsOptions
+  //       icon={<SvgIcon sx={{ width: 28, height: 28 }}>{settingIcons.siderbarDuotone}</SvgIcon>}
+  //       options={(Object.keys(primaryColorPresets) as SettingsState['primaryColor'][]).map(
+  //         (key) => ({
+  //           name: key,
+  //           value: primaryColorPresets[key].main,
+  //         })
+  //       )}
+  //       value={settings.state.primaryColor}
+  //       onChangeOption={(newOption) => {
+  //         settings.setState({ primaryColor: newOption });
+  //       }}
+  //     />
+  //   </LargeBlock>
+  // );
 
-  const renderNav = () => (
-    <LargeBlock title="Nav" tooltip="Dashboard only" sx={{ gap: 2.5 }}>
-      {visibility.navLayout && (
-        <SmallBlock
-          label="Layout"
-          canReset={settings.state.navLayout !== defaultSettings.navLayout}
-          onReset={() => {
-            settings.setState({ navLayout: defaultSettings.navLayout });
-          }}
-        >
-          <NavLayoutOptions
-            value={settings.state.navLayout}
-            onChangeOption={(newOption) => {
-              settings.setState({ navLayout: newOption });
-            }}
-            options={[
-              {
-                value: 'vertical',
-                icon: (
-                  <SvgIcon sx={{ width: 1, height: 'auto' }}>{settingIcons.navVertical}</SvgIcon>
-                ),
-              },
-              {
-                value: 'horizontal',
-                icon: (
-                  <SvgIcon sx={{ width: 1, height: 'auto' }}>{settingIcons.navHorizontal}</SvgIcon>
-                ),
-              },
-              {
-                value: 'mini',
-                icon: <SvgIcon sx={{ width: 1, height: 'auto' }}>{settingIcons.navMini}</SvgIcon>,
-              },
-            ]}
-          />
-        </SmallBlock>
-      )}
-      {visibility.navColor && (
-        <SmallBlock
-          label="Color"
-          canReset={settings.state.navColor !== defaultSettings.navColor}
-          onReset={() => {
-            settings.setState({ navColor: defaultSettings.navColor });
-          }}
-        >
-          <NavColorOptions
-            value={settings.state.navColor}
-            onChangeOption={(newOption) => {
-              settings.setState({ navColor: newOption });
-            }}
-            options={[
-              {
-                label: 'Integrate',
-                value: 'integrate',
-                icon: <SvgIcon>{settingIcons.sidebarOutline}</SvgIcon>,
-              },
-              {
-                label: 'Apparent',
-                value: 'apparent',
-                icon: <SvgIcon>{settingIcons.sidebarFill}</SvgIcon>,
-              },
-            ]}
-          />
-        </SmallBlock>
-      )}
-    </LargeBlock>
-  );
+  // const renderNav = () => (
+  //   <LargeBlock title="Nav" tooltip="Dashboard only" sx={{ gap: 2.5 }}>
+  //     {visibility.navLayout && (
+  //       <SmallBlock
+  //         label="Layout"
+  //         canReset={settings.state.navLayout !== defaultSettings.navLayout}
+  //         onReset={() => {
+  //           settings.setState({ navLayout: defaultSettings.navLayout });
+  //         }}
+  //       >
+  //         <NavLayoutOptions
+  //           value={settings.state.navLayout}
+  //           onChangeOption={(newOption) => {
+  //             settings.setState({ navLayout: newOption });
+  //           }}
+  //           options={[
+  //             {
+  //               value: 'vertical',
+  //               icon: (
+  //                 <SvgIcon sx={{ width: 1, height: 'auto' }}>{settingIcons.navVertical}</SvgIcon>
+  //               ),
+  //             },
+  //             {
+  //               value: 'horizontal',
+  //               icon: (
+  //                 <SvgIcon sx={{ width: 1, height: 'auto' }}>{settingIcons.navHorizontal}</SvgIcon>
+  //               ),
+  //             },
+  //             {
+  //               value: 'mini',
+  //               icon: <SvgIcon sx={{ width: 1, height: 'auto' }}>{settingIcons.navMini}</SvgIcon>,
+  //             },
+  //           ]}
+  //         />
+  //       </SmallBlock>
+  //     )}
+  //     {visibility.navColor && (
+  //       <SmallBlock
+  //         label="Color"
+  //         canReset={settings.state.navColor !== defaultSettings.navColor}
+  //         onReset={() => {
+  //           settings.setState({ navColor: defaultSettings.navColor });
+  //         }}
+  //       >
+  //         <NavColorOptions
+  //           value={settings.state.navColor}
+  //           onChangeOption={(newOption) => {
+  //             settings.setState({ navColor: newOption });
+  //           }}
+  //           options={[
+  //             {
+  //               label: 'Integrate',
+  //               value: 'integrate',
+  //               icon: <SvgIcon>{settingIcons.sidebarOutline}</SvgIcon>,
+  //             },
+  //             {
+  //               label: 'Apparent',
+  //               value: 'apparent',
+  //               icon: <SvgIcon>{settingIcons.sidebarFill}</SvgIcon>,
+  //             },
+  //           ]}
+  //         />
+  //       </SmallBlock>
+  //     )}
+  //   </LargeBlock>
+  // );
 
-  const renderFont = () => (
-    <LargeBlock title="Font" sx={{ gap: 2.5 }}>
-      {visibility.fontFamily && (
-        <SmallBlock
-          label="Family"
-          canReset={settings.state.fontFamily !== defaultSettings.fontFamily}
-          onReset={() => {
-            settings.setState({ fontFamily: defaultSettings.fontFamily });
-          }}
-        >
-          <FontFamilyOptions
-            value={settings.state.fontFamily}
-            onChangeOption={(newOption) => {
-              settings.setState({ fontFamily: newOption });
-            }}
-            options={[
-              themeConfig.fontFamily.primary,
-              'Inter Variable',
-              'DM Sans Variable',
-              'Nunito Sans Variable',
-            ]}
-            icon={<SvgIcon sx={{ width: 28, height: 28 }}>{settingIcons.font}</SvgIcon>}
-          />
-        </SmallBlock>
-      )}
-      {visibility.fontSize && (
-        <SmallBlock
-          label="Size"
-          canReset={settings.state.fontSize !== defaultSettings.fontSize}
-          onReset={() => {
-            settings.setState({ fontSize: defaultSettings.fontSize });
-          }}
-          sx={{ gap: 5 }}
-        >
-          <FontSizeOptions
-            options={[12, 20]}
-            value={settings.state.fontSize}
-            onChangeOption={(newOption) => {
-              settings.setState({ fontSize: newOption });
-            }}
-          />
-        </SmallBlock>
-      )}
-    </LargeBlock>
-  );
+  // const renderFont = () => (
+  //   <LargeBlock title="Font" sx={{ gap: 2.5 }}>
+  //     {visibility.fontFamily && (
+  //       <SmallBlock
+  //         label="Family"
+  //         canReset={settings.state.fontFamily !== defaultSettings.fontFamily}
+  //         onReset={() => {
+  //           settings.setState({ fontFamily: defaultSettings.fontFamily });
+  //         }}
+  //       >
+  //         <FontFamilyOptions
+  //           value={settings.state.fontFamily}
+  //           onChangeOption={(newOption) => {
+  //             settings.setState({ fontFamily: newOption });
+  //           }}
+  //           options={[
+  //             themeConfig.fontFamily.primary,
+  //             'Inter Variable',
+  //             'DM Sans Variable',
+  //             'Nunito Sans Variable',
+  //           ]}
+  //           icon={<SvgIcon sx={{ width: 28, height: 28 }}>{settingIcons.font}</SvgIcon>}
+  //         />
+  //       </SmallBlock>
+  //     )}
+  //     {visibility.fontSize && (
+  //       <SmallBlock
+  //         label="Size"
+  //         canReset={settings.state.fontSize !== defaultSettings.fontSize}
+  //         onReset={() => {
+  //           settings.setState({ fontSize: defaultSettings.fontSize });
+  //         }}
+  //         sx={{ gap: 5 }}
+  //       >
+  //         <FontSizeOptions
+  //           options={[12, 20]}
+  //           value={settings.state.fontSize}
+  //           onChangeOption={(newOption) => {
+  //             settings.setState({ fontSize: newOption });
+  //           }}
+  //         />
+  //       </SmallBlock>
+  //     )}
+  //   </LargeBlock>
+  // );
 
-  return (
-    <Drawer
-      anchor="right"
-      open={settings.openDrawer}
-      onClose={settings.onCloseDrawer}
-      slotProps={{
-        backdrop: { invisible: true },
-        paper: {
-          sx: [
-            (theme) => ({
-              ...theme.mixins.paperStyles(theme, {
-                color: varAlpha(theme.vars.palette.background.defaultChannel, 0.9),
-              }),
-              width: 360,
-            }),
-            ...(Array.isArray(sx) ? sx : [sx]),
-          ],
-        },
-      }}
-    >
-      {renderHead()}
+  // return (
+  //   <Drawer
+  //     anchor="right"
+  //     open={settings.openDrawer}
+  //     onClose={settings.onCloseDrawer}
+  //     slotProps={{
+  //       backdrop: { invisible: true },
+  //       paper: {
+  //         sx: [
+  //           (theme) => ({
+  //             ...theme.mixins.paperStyles(theme, {
+  //               color: varAlpha(theme.vars.palette.background.defaultChannel, 0.9),
+  //             }),
+  //             width: 360,
+  //           }),
+  //           ...(Array.isArray(sx) ? sx : [sx]),
+  //         ],
+  //       },
+  //     }}
+  //   >
+  //     {renderHead()}
 
       <Scrollbar>
         <Box
