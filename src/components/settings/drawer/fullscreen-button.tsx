29f17a24a8a19e560afbddc23b047211648e16b1
Yannick Dudu <dushimeyannick54@gmail.com>
2026-08-31 16:52:52 +0300

    Update fullscreen-button.tsx

--- src/components/settings/drawer/fullscreen-button.tsx
+++ src/components/settings/drawer/fullscreen-button.tsx
@@ -1,38 +1,38 @@
-'use client';
+// 'use client';
 
-import { useState, useCallback } from 'react';
+// import { useState, useCallback } from 'react';
 
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
+// import Tooltip from '@mui/material/Tooltip';
+// import IconButton from '@mui/material/IconButton';
 
-import { Iconify } from '../../iconify';
+// import { Iconify } from '../../iconify';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export function FullScreenButton() {
-  const [fullscreen, setFullscreen] = useState(false);
+// export function FullScreenButton() {
+//   const [fullscreen, setFullscreen] = useState(false);
 
-  const handleToggleFullscreen = useCallback(() => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-      setFullscreen(true);
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-      setFullscreen(false);
-    }
-  }, []);
+//   const handleToggleFullscreen = useCallback(() => {
+//     if (!document.fullscreenElement) {
+//       document.documentElement.requestFullscreen();
+//       setFullscreen(true);
+//     } else if (document.exitFullscreen) {
+//       document.exitFullscreen();
+//       setFullscreen(false);
+//     }
+//   }, []);
 
-  return (
-    <Tooltip title={fullscreen ? 'Exit' : 'Fullscreen'}>
-      <IconButton onClick={handleToggleFullscreen} color={fullscreen ? 'primary' : 'default'}>
-        <Iconify
-          icon={
-            fullscreen
-              ? 'solar:quit-full-screen-square-outline'
-              : 'solar:full-screen-square-outline'
-          }
-        />
-      </IconButton>
-    </Tooltip>
-  );
-}
+//   return (
+//     <Tooltip title={fullscreen ? 'Exit' : 'Fullscreen'}>
+//       <IconButton onClick={handleToggleFullscreen} color={fullscreen ? 'primary' : 'default'}>
+//         <Iconify
+//           icon={
+//             fullscreen
+//               ? 'solar:quit-full-screen-square-outline'
+//               : 'solar:full-screen-square-outline'
+//           }
+//         />
+//       </IconButton>
+//     </Tooltip>
+//   );
+// }
