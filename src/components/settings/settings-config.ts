e488f9a782de5107a66fd76a7c618140852cdb56
Yannick Dudu <dushimeyannick54@gmail.com>
2026-08-31 17:15:15 +0300

    Update settings-config.ts

--- src/components/settings/settings-config.ts
+++ src/components/settings/settings-config.ts
@@ -1,11 +1,11 @@
 import type { SettingsState } from './types';
 
-import { CONFIG } from 'src/global-config';
-import { themeConfig } from 'src/theme/theme-config';
+// import { CONFIG } from 'src/global-config';
+// import { themeConfig } from 'src/theme/theme-config';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export const SETTINGS_STORAGE_KEY: string = 'app-settings';
+// export const SETTINGS_STORAGE_KEY: string = 'app-settings';
 
 export const defaultSettings: SettingsState = {
   mode: themeConfig.defaultMode,
@@ -17,5 +17,5 @@ export const defaultSettings: SettingsState = {
   // compactLayout: true,
   // fontSize: 16,
   // fontFamily: themeConfig.fontFamily.primary,
-  version: CONFIG.appVersion,
+  // version: CONFIG.appVersion,
 };
